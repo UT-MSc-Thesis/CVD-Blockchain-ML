b81b6cf75e6abbf93b9feb239464d14a25f9f370
main <null>
2023-12-06 23:07:51 +0330

    test: add unit test for dummy contract

--- contracts/registry/src/contract.rs
+++ contracts/registry/src/contract.rs
@@ -12,6 +12,11 @@ pub fn instantiate(
     Ok(Response::new())
 }
 
+#[allow(dead_code)]
+pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> StdResult<Response> {
+    unimplemented!()
+}
+
 pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     use QueryMsg::*;
 
@@ -29,5 +34,44 @@ mod query {
         };
 
         Ok(resp)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Addr;
+    use cw_multi_test::{App, ContractWrapper, Executor};
+
+    use super::*;
+
+    #[test]
+    fn greet_query() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &Empty {},
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: GreetResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::Greet {})
+            .unwrap();
+
+        assert_eq!(
+            resp,
+            GreetResp {
+                message: "Hello World".to_owned()
+            }
+        );
     }
 }
